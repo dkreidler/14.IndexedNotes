2574075b3584e6197e5151bc129795f662efd230
dkreidler <dkreidler@gmail.com>
2018-08-25 18:22:34 -0400

    More code
    (but the preventDefault is throwing an error in the console.log... )

--- index.js
+++ index.js
@@ -90,3 +90,93 @@ window.onload = function() {
         };
     }
 };
+
+// define the displayData() function
+function displayData() {
+    // Here we empty the contents of the list element each time the display is updated
+    // Without this, we'd get duplicates listed each time a note is added
+    while (list.firstChild) {
+        list.removeChild(list.firstChild);
+    }
+
+    // open object store and then get a cursor - which iterates through
+    // all the different data items in the store
+    let objectStore = db.transaction('notes').objectStore('notes');
+    objectStore.openCursor().onsuccess = function(e) {
+        // Get a reference to the cursor
+        let cursor = e.target.result;
+
+        // if there is still another data item to iterate through, keep this code
+        if (cursor) {
+            // create a list item, h3, and p to put each data item inside when displaying it
+            // structure the HTML fragment, and append it inside the list
+            let listItem = document.createElement('li');
+            let h3 = document.createElement('h3');
+            let para = document.createElement('p');
+
+            listItem.appendChild(h3);
+            listItem.appendChild(para);
+            listItem.appendChild(listItem);
+
+            // pu the data from the cursor inside the h3 and para
+            h3.textContent = cursor.value.title;
+            para.textContent = cursor.value.body;
+
+            // store the ID of the data item inside an attribute on the listItem so we know
+            // which item it corresponds to. This will be useful later when we want to delete items
+            listItem.setAttribute('data-note-id', cursor.value.id);
+
+            // Create a button and place it inside each listItem
+            let deleteBtn = document.createElement('button');
+            listItem.appendChild(deleteBtn);
+            deleteBtn.textContent = 'Delete';
+
+            // set an event handler so that when the button is clicked, the deleteitem()
+            // function is run
+            deleteBtn.onclick = deleteItem;
+
+            // Iterate to the next item in the cursor
+            cursor.continue();
+        } else {
+            // Again, if list item is empty, display a 'No notes stored' messaage
+            if (!list.firstChild) {
+                let listItem = document.createElement('li');
+                listItem.textContent = 'No notes stored.';
+                list.appendChild(listItem);
+            }
+            // if there are no more cursor items to iterate though, say so
+            console.log('Notes all displayed');
+        }
+
+    };
+
+}
+
+// define deleteItem() function
+function deleteItem(e) {
+    // retrieve the name of the task we want to delete. We need to convert
+    // it to a number before trying to use it with IDB; IDB key values are
+    // type-sensitive.
+    let noteId = Number(e.target.parentNode.getAttribute('data-note-id'));
+
+    // open a database transaction and delete the task, finding it using 
+    // the id we retrieved above
+    let transaction = db.transaction(['notes'], 'readwrite');
+    let objectStore = transaction.objectStore('notes');
+    let request = objectStore.delete(noteId);
+
+    // report that the data item has been deleted
+    transaction.oncomplete = function() {
+        // delete the parent of the button
+        // which is the list item, so it is no longer displayed
+        e.target.parentNode.parentNode.removeChild(e.target.parentNode);
+        console.log('Note ' + noteId + ' deleted.');
+
+        // Again, if the list item is empty, dispaly a 'No notes stored' message
+        if (!list.firstChild) {
+            let listItem = document.createElement('li');
+            listItem.textContent = 'No notes stored.';
+            list.appendChild(listItem);
+        }
+    };
+}
